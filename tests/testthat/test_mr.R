b37f201d1c6b9a08bca92a56dc4c5b6e2edf960c
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2024-04-07 19:35:46 +0100

    Updating mr test with eqtlgen

--- tests/testthat/test_mr.R
+++ tests/testthat/test_mr.R
@@ -1,11 +1,16 @@
 mr_results <- tempfile(fileext = ".tsv")
 
 test_that("mr.perform_mr_on_pqtl_datasets runs cis only mr against a gwas", {
-  perform_mr_on_pqtl_datasets("data/test_data_small.tsv.gz", mr_results, qtl_dir = "data")
+  local_mock(
+    list.files = function(dir, pattern, full.names) {
+      return("data/some_eqtlgen_hits.tsv.gz")
+    }
+  )
+  perform_mr_on_eqtlgen_datasets("data/test_data_small.tsv.gz", results_output = mr_results)
 
   expect_true(file.exists(mr_results))
   results <- vroom::vroom(mr_results, show_col_types=F)
-  expect_equal(nrow(results), 12)
+  expect_equal(nrow(results), 4)
 })
 
 test_that("mr.compare_interesting_mr_results creates a grouped forest plot", {
