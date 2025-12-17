4d0e63ec914f3e86c251a68f55888859cc20b44b
Andrew Elmore <andrew.elmore@bristol.ac.uk>
2025-12-17 17:51:04 +0000

    updating liftover dir

--- R/constants.R
+++ R/constants.R
@@ -1,4 +1,4 @@
-get_env_var <- function(env_var_name, default_value) {
+get_env_var <- function(env_var_name, default_value=NULL) {
   if (Sys.getenv(env_var_name) == "") {
     return(default_value)
   } else {
@@ -10,11 +10,11 @@ user_data_dir <- get_env_var("DATA_DIR", "")
 user_results_dir <- get_env_var("RESULTS_DIR", "")
 
 number_of_cpus_available <- as.numeric(get_env_var("SLURM_CPUS_ON_NODE", 1))
-genomic_data_dir <- get_env_var("GENOMIC_DATA_DIR", "/mnt/storage/private/mrcieu/data/genomic_data/")
-thousand_genomes_dir <- get_env_var("THOUSAND_GENOMES_DIR", "/mnt/storage/private/mrcieu/data/genomic_data/1000genomes/b37_dbsnp156/")
-qtl_directory <- get_env_var("QTL_DIRECTORY", "/mnt/storage/private/mrcieu/data/qtl_datasets/")
-liftover_dir <- get_env_var("LIFTOVER_DIR", "/mnt/storage/private/mrcieu/data/genomic_data/liftover/")
+genomic_data_dir <- get_env_var("GENOMIC_DATA_DIR", "")
+thousand_genomes_dir <- get_env_var("THOUSAND_GENOMES_DIR", "")
+qtl_directory <- get_env_var("QTL_DIRECTORY", "")
 
+liftover_dir <- paste0(genomic_data_dir, '/liftover')
 pqtl_top_hits_dir <- paste0(qtl_directory, "/pqtl")
 metabrain_top_hits_dir <- paste0(qtl_directory, "/metabrain/top_hits")
 metabrain_gwas_dir <- paste0(qtl_directory, "/metabrain/gwas")
